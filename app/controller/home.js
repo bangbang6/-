a4a8aad9294e6027228789db513c83285b9218d4
bangbang6 <577174667@qq.com>
2021-01-13 11:17:38 +0800

    feat(home.js): test husky

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
-    console.log('xxxxx')  ;
+    console.log('xxxxxx');
     ctx.body = 'hi, egg';
   }
 }
